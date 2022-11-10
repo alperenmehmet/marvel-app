d10996179aba0a2f1ada7a6f438b5442e1b2f299
Mehmet ALPEREN <me.alperen@gmail.com>
2022-11-10 19:28:40 +0300

    context api added.

---
+++ context/context.tsx
@@ -0,0 +1,23 @@
+import React, {
+  createContext,
+  FC,
+  ReactNode,
+  useContext,
+  useState
+} from 'react'
+
+export type MarvelContext = {
+  children?: React.ReactNode
+  name?: string
+}
+
+const AppContext = createContext({} as MarvelContext)
+
+export const AppProvider: FC<MarvelContext> = ({ children }) => {
+  const [name, setName] = useState<string>('hello')
+  return <AppContext.Provider value={{ name }}>{children}</AppContext.Provider>
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { Navbar } from '../components/navbar'
+import { useGlobalContext } from '../context/context'
 
 export default function Home() {
+  const { name } = useGlobalContext()
+  console.log(name)
   return (
     <div>
       <Navbar />
