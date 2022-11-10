698189dadd3010bc8c8d2289df2f8f9df776639c
Mehmet ALPEREN <me.alperen@gmail.com>
2022-11-10 15:09:32 +0300

    project initialized.

---
+++ components/navbar/index.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Navbar = () => {
+  return <Wrapper>here is navbar</Wrapper>
+}
+
+const Wrapper = styled.nav`
+  height: 52px;
+  background: red;
+`
